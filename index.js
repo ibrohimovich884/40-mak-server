be98a5a45a75122d84fd0a06e0df6398c5625523
ibrohimovich884 <bkbekmirzayev@gmail.com>
2025-10-03 21:22:54 +0500

    1

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Server ishga tushdi");
+});
+
+
 app.get("/hez", (req, res) => {
   res.send("Xush kelibsiz! Bu Hez sahifasi 👋");
 });
